0cc721ec2a5bccf2398a1301f55a56acf4335926
Oleh Kirich <olehkirich@MacBook-Air-Oleh.local>
2025-09-04 08:29:42 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import '../../App.scss';
 
 export function Person({ person }) {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const { name, sex, isMarried, partnerName } = person;
+
+  // вычисляем роль партнёра заранее
+  const partnerRole = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <div className="Person">
       <p className="Person__name">My name is {name}</p>
 
-      {age && <p className="Person__age">I am {age}</p>}
+      {'age' in person && <p className="Person__age">I am {person.age}</p>}
 
       {isMarried ? (
         <p className="Person__partner">
-          {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
+          {partnerName} is my {partnerRole}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
